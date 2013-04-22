e1c467caa6414314bfd3a0e40bf4b3eecd667cf5
Marcel Laurijsse <m.laurijsse@gmail.com>
2013-04-22 14:27:50 +0200

    With Commthread

--- main.cpp
+++ main.cpp
@@ -47,8 +47,8 @@ int main(int argc, char * argv[])	{
 	viewer->realize();
 	
 	// start communicator thread
-	//CommunicatorThread *commThread = new CommunicatorThread((CustomCamera *) viewer->getCameraManipulator());
-	//commThread->start();
+	CommunicatorThread *commThread = new CommunicatorThread((CustomCamera *) viewer->getCameraManipulator());
+	commThread->start();
 	
 	// calculate frametime
 	double minFrameTime = 1.0 / FRAMERATE;
@@ -65,7 +65,7 @@ int main(int argc, char * argv[])	{
 			OpenThreads::Thread::microSleep(1000000.0 * (minFrameTime - frameTime));
 	}
 	
-	//commThread->stop();
+	commThread->stop();
 	
 	return 0;
 }
